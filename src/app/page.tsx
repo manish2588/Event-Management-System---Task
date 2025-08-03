f8f1af58d5d2f9c563b968d97a7001b0a8cb226a
manish2588 <manishkc258@gmail.com>
2025-08-03 15:30:34 +0545

    Add Home Page Content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,154 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useReducer, useState } from "react";
+import { CalendarPlus } from "lucide-react";
+import { EventList } from "./components/EventList";
+import { EventFilter } from "./components/EventFilter";
+import dummyEvents from "./DummyEvent/DummyEvent";
+import { eventReducer } from "./reducer/eventReducer";
+import { Modal } from "./components/Modal";
+import EventForm from "./components/form/EventForm";
+import { Button } from "@/components/ui/button";
+import { Event } from "./utils/types";
+import Banner from "./components/Banner";
+import dynamic from "next/dynamic";
+import { Calendar, Grid3X3 } from "lucide-react";
 
-export default function Home() {
+const CalendarView = dynamic(
+  () =>
+    import("../app/components/CalenderView").then((mod) => mod.CalendarView),
+  { ssr: false }
+);
+
+const LOCAL_STORAGE_KEY = "events";
+
+const HomePage: React.FC = () => {
+  const [events, dispatch] = useReducer(eventReducer, []);
+  const [filteredEvents, setFilteredEvents] = useState<Event[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingEvent, setEditingEvent] = useState<Event | null>(null);
+  const [viewMode, setViewMode] = useState<"list" | "calendar">("list"); // New state for view mode
+
+
+  {/* Initially save dummy events on local storage */}
+  useEffect(() => {
+    const stored = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (stored) {
+      const parsedEvents = JSON.parse(stored);
+      dispatch({ type: "INIT", payload: parsedEvents });
+      setFilteredEvents(parsedEvents);
+    } else {
+      dispatch({ type: "INIT", payload: dummyEvents });
+      setFilteredEvents(dummyEvents);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(events));
+    setFilteredEvents(events);
+  }, [events]);
+
+  const handleEditClick = (event: Event) => {
+    setEditingEvent(event);
+    setIsModalOpen(true);
+  };
+
+  const handleVenueFilter = (venue: string | null) => {
+    if (!venue || venue === "all") {
+      setFilteredEvents(events);
+    } else {
+      setFilteredEvents(events.filter((event) => event.venue === venue));
+    }
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <Banner />
+      {/* Heading */}
+      <div className="max-w-7xl mx-auto p-6 ">
+        <h1 className="text-gray-900 text-2xl md:text-4xl font-semibold drop-shadow-lg mb-8">
+          Event Management System - EB Pearls
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex flex-col md:flex-row justify-between items-start sm:items-center gap-4 mb-10">
+          <h1 className="text-2xl md:text-3xl font-medium">
+            {viewMode === "list" ? "Events List" : "Calendar View"}
+          </h1>
+          {/* Select Filter and Buttons */}
+          <div className="flex flex-col md:flex-row gap-4 w-1/2 md:w-auto ">
+            <EventFilter onVenueChange={handleVenueFilter} />
+            <Button
+              onClick={() =>
+                setViewMode(viewMode === "list" ? "calendar" : "list")
+              }
+              variant="outline"
+              className="gap-2 flex items-center"
+            >
+              {viewMode === "list" ? (
+                <>
+                  <Calendar className="w-4 h-4 text-green-500" />
+                  Calendar View
+                </>
+              ) : (
+                <>
+                  <Grid3X3 className="w-4 h-4 text-green-500" />
+                  List View
+                </>
+              )}
+            </Button>
+
+            <Button
+              onClick={() => {
+                setEditingEvent(null);
+                setIsModalOpen(true);
+              }}
+              className="gap-2 bg-green-500 hover:bg-green-600"
+            >
+              <CalendarPlus className="h-5 w-5" />
+              Add Event
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+         {/* Calender View and List View*/}
+        {viewMode === "calendar" ? (
+          <CalendarView
+            events={filteredEvents}
+            onEventClick={(event) => {
+              setEditingEvent(event);
+              setIsModalOpen(true);
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        ) : (
+          <EventList
+            events={filteredEvents}
+            dispatch={dispatch}
+            onEdit={handleEditClick}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        )}
+
+           {/*Form modal for event add and delete */}
+        <Modal
+          open={isModalOpen}
+          onOpenChange={(open) => {
+            setIsModalOpen(open);
+            if (!open) setEditingEvent(null);
+          }}
+          title={editingEvent ? "Edit Event" : "Add New Event"}
+          width="lg"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <EventForm
+            dispatch={dispatch}
+            onClose={() => {
+              setIsModalOpen(false);
+              setEditingEvent(null);
+            }}
+            events={events}
+            editingEvent={editingEvent}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </Modal>
+      </div>
+    </>
   );
-}
+};
+
+export default HomePage;
